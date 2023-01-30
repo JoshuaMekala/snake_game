42173ebed812d47abe03b91d439c15b793401556
JoshuaMekala <mekala.joshua@gmail.com>
2023-01-30 13:21:59 -0500

    able to hold high score from previous runs

--- scoreboard.py
+++ scoreboard.py
@@ -11,7 +11,8 @@ class Scoreboard(Turtle):
         self.color("white")
         self.setposition(0,270)
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.update_score()
 
 
@@ -22,6 +23,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt",mode ="w") as data:
+                data.write(f"{self.high_score}")
+
         self.score = 0
         self.update_score()
 
